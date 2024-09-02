56232e87a4a6016e1bc1b26b7970fa5b6ccffc25
Solon Chan <147296165+siychan@users.noreply.github.com>
2024-09-02 10:20:00 -0700

    Update String1.java
    completed make abba

--- src/main/java/String1.java
+++ src/main/java/String1.java
@@ -50,7 +50,7 @@ public class String1
      * makeAbba("What", "Up") → "WhatUpUpWhat"
      */
     public String makeAbba(String a, String b) {
-        return unimplemented;
+        return a + b + b + a;
     }
 
     /*
